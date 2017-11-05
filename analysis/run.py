ebcdb551c5bb2e5d59535ce329c935255987afcd
Vojtěch Jelínek <vojtin.j@gmail.com>
2017-11-05 18:04:30 +0100

    initial commit

---
+++ analysis/run.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+def laod_data():
+    pass
+
+def run():
+    load_data()
+    #pprint(city_map.map)
+
+if __name__ == "__main__":
+    run()
